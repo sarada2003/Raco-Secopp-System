3196a71542cc54322dad736927e13f4fb352a93f
Sam <samuelrada2712@gmail.com>
2025-12-08 21:52:40 -0500

    Inicio del Backend del proyecto

---
+++ backend/sistemawebraco/src/main/java/co/edu/unbosque/sistemawebraco/SistemawebracoApplication.java
@@ -0,0 +1,13 @@
+package co.edu.unbosque.sistemawebraco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemawebracoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemawebracoApplication.class, args);
+	}
+
+}
---
+++ backend/sistemawebraco/src/test/java/co/edu/unbosque/sistemawebraco/SistemawebracoApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.unbosque.sistemawebraco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemawebracoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
